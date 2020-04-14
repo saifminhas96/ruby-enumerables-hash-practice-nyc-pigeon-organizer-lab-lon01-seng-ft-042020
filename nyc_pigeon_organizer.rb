786678ec3790044403e57f9482bcb216cdaedf35
Saif Minhas <saif_minhas@hotmail.com>
2020-04-14 09:54:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ new_hash = {}
+ 
+ data.each do |key, value|
+
+value.each do |describe, array|
+ 
+ array.each do |name|
+
+if new_hash.has_key?(name)
+    
+  if new_hash[name].has_key?(key)
+    
+    new_hash[name][key] << describe.to_s
+ else
+ new_hash[name][key] = [describe.to_s]
+    end
+     else
+  new_hash[name] = {key => [describe.to_s]}
+        end
+      end
+    end
+  end
+  new_hash
 end
+ 
+
